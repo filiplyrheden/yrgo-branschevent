cea03970d90965fee4484245dd1dad53773f712d
filiplyrheden <filip.lyrheden@gmail.com>
2025-03-21 11:49:18 +0100

    add Header component and integrate it into the routing structure; remove unused SVG asset

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={
+          <>
+            <Header />
+            <Home />
+          </>
+        } />
         <Route path="/about" element={<About />} />
       </Routes>
     </>
