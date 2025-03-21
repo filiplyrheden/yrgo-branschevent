b5367c8be0fc8fcf1eeae657577c1bb9ed70e67e
filiplyrheden <filip.lyrheden@gmail.com>
2025-03-21 13:55:07 +0100

    Hero component layout and styles; integrate new font and SVG elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,61 @@
-import React from 'react';
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
-return (
-<section className='hero'>
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <div className="hero-text-container">
+          <div className="hero-text">
+            <h2 className="hero-title">Dev & Designer Mingel</h2>
+            <div className="date-and-arrow">
+            <p className="hero-date">23.04.25</p>
 
-</section>
-);
+<div className="arrow-wrapper">
+            <svg className="hero-arrow"
+              xmlns="http://www.w3.org/2000/svg"
+              width="77"
+              height="77"
+              viewBox="0 0 77 77"
+              fill="none"
+            >
+              <path
+                d="M28.6378 51.5442H50.9117V29.2703"
+                stroke="white"
+                stroke-width="7"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M50.9117 51.5442L25.4559 26.0883"
+                stroke="white"
+                stroke-width="7"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            </div>
+            </div>
+          </div>
+
+        </div>
+        <div className="hero-icons">
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+          </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
