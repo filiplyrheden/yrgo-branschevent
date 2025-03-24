60ba98c70c006701cac05938815e3da25f5e1086
filiplyrheden <filip.lyrheden@gmail.com>
2025-03-24 11:54:26 +0100

    add styled-components and create Input component for LandingForm

---
+++ src/components/Input.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  display: flex;
+  width: 18.48rem;
+  height: 3rem;
+  border-radius: 0.25rem;
+  border: 1px solid #f2f2f2;
+  background: #f2f2f2;
+  padding: 0.5rem;
+  font-size: 1rem;
+`;
+
+const Input = ({ id = "name", label = "Namn:", placeholder = "Namn..." }) => {
+  return (
+    <div>
+      <label htmlFor={id}>{label}</label>
+      <StyledInput type="text" id={id} placeholder={placeholder} />
+    </div>
+  );
+};
+
+export default Input;
