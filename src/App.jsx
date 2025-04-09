3d3cf8a81dc3229d1b14bd5de0cc9fafbf79a4b7
filiplyrheden <filip.lyrheden@gmail.com>
2025-04-09 13:37:54 +0200

    Clean up App.jsx by removing unnecessary whitespace and improving code formatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
-
-
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer.jsx";
 import "./styles/Global.css";
-
-// Importera dina komponenter
 import Home from "./pages/Home";
 import CompanyProfile from "./components/CompanyProfile";
 import StudentProfile from "./components/StudentProfile";
@@ -19,32 +15,32 @@ const ProfilePage = () => {
   const [loading, setLoading] = useState(true);
   const [userType, setUserType] = useState(null);
   const [authenticated, setAuthenticated] = useState(false);
-  
+
   useEffect(() => {
     const checkUser = async () => {
       const { data } = await supabase.auth.getSession();
-      
+
       if (data.session) {
         setAuthenticated(true);
         // Hämta användartyp från metadata
         const userType = data.session.user.user_metadata?.user_type;
         setUserType(userType);
       }
-      
+
       setLoading(false);
     };
-    
+
     checkUser();
   }, []);
-  
+
   if (loading) {
     return <div>Laddar...</div>;
   }
-  
+
   if (!authenticated) {
     return <Navigate to="/" />;
   }
-  
+
   return userType === "Företag" ? <CompanyProfile /> : <StudentProfile />;
 };
 
@@ -52,35 +48,40 @@ const ProfilePage = () => {
 const ProtectedRoute = ({ element }) => {
   const [loading, setLoading] = useState(true);
   const [authenticated, setAuthenticated] = useState(false);
-  
+
   useEffect(() => {
     const checkAuth = async () => {
       const { data } = await supabase.auth.getSession();
       setAuthenticated(!!data.session);
       setLoading(false);
     };
-    
+
     checkAuth();
   }, []);
-  
+
   if (loading) {
     return <div>Laddar...</div>;
   }
-  
+
   return authenticated ? element : <Navigate to="/" />;
 };
-
 
 function App() {
   return (
     <Routes>
-      
-      <Route path="/" element={<> <Header/>
-      <Home/>
-      <Footer/> </>} />
+      <Route
+        path="/"
+        element={
+          <>
+            {" "}
+            <Header />
+            <Home />
+            <Footer />{" "}
+          </>
+        }
+      />
       <Route path="/profil" element={<ProfilePage />} />
       <Route path="/swajp" element={<ProtectedRoute element={<Swipe />} />} />
-      
     </Routes>
   );
 }
