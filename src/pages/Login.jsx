b9bd022aee3197148ed1900e316b904443e6c91c
filiplyrheden <filip.lyrheden@gmail.com>
2025-04-10 11:33:39 +0200

    Add Login component with input fields for email and password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,32 @@
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
+import Input from "../components/ui/Input";
+import Button from "../components/ui/Button";
+import "./Login.css";
 
 export default function Login() {
   return (
     <div>
       <Header />
-
+      <div className="input-container">
+        <Input
+          id="email"
+          label="Mejl"
+          placeholder="Mejl..."
+          type="email"
+          required
+        />
+        <div className="input-container">
+          <Input
+            id="password"
+            label="Lösenord"
+            placeholder="Lösenord..."
+            type="password"
+            required
+          />
+        </div>
+      </div>
+      <Button text="Logga in" />
       <Footer />
     </div>
   );
